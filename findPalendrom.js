68e3b7d13f530226a736f96af31995b272d54168
theshahzaib5 <theshahzaib5@gmail.com>
2019-10-01 14:44:12 +0500

    updated palendrom with reverse function

--- findPalendrom.js
+++ findPalendrom.js
@@ -1,10 +1,14 @@
-function findPalendrom(myString) {
+function reverse(myString) {
   let newString = "";
   for (let index = myString.length - 1; index >= 0; index--)
     newString += myString[index];
+  return newString;
+}
 
+function findPalendrom(myString) {
+  let newString = reverse(myString);
   return newString === myString ? true : false;
 }
 
-console.log(findPalendrom("Hello"));
+console.log(findPalendrom("abba"));
 // console.log(findPalendrom("abca"));
