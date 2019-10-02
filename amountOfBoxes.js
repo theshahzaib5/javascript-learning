25cd50a95e104fc622765c3d094018947ea7e52f
theshahzaib5 <theshahzaib5@gmail.com>
2019-10-02 15:11:31 +0500

    added amount of boxes function

---
+++ amountOfBoxes.js
@@ -0,0 +1,22 @@
+// Create a function that takes a number (step) as an argument and returns the amount of boxes in that step of the sequence.
+
+// Step 0: Start with 0
+// Step 1: Add 3
+// Step 2: Subtract 1
+// Repeat Step 1 & 2 ...
+
+function boxSeq(number) {
+  let boxes = 0;
+  for (let index = 1; index <= number; index++) {
+    if (index % 2 === 0) boxes--;
+    else if (index % 2 !== 0) boxes = boxes + 3;
+  }
+  return boxes;
+}
+
+// function boxSeq(number) {
+//   if (number % 2 === 0) return number;
+//   return number + 2;
+// }
+
+console.log(boxSeq(5));
